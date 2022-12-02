ed176a66b9b55fb2d8fb2bcc3b3d90076e8ee425
Jonas Johann <jonas.johann.jj@gmail.com>
2022-12-02 10:12:31 +0100

    init master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,130 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material/dialog";
+import {HttpClient} from "@angular/common/http";
+import {DOCUMENT} from "@angular/common";
+import teams from "../assets/teams.json";
+
+export interface DialogData {
+  newTeamName: String;
+  teams: any;
+  selectedTeam: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public dialog: MatDialog
+  ) {
+  }
+
+  selectedTeam;
+
+  ngOnInit(): void {
+    this.selectedTeam = teams[0];
+    let elem = this.document;
+    elem.body.onkeydown = (event) => {
+      if (event.key == 'ArrowLeft') {
+        this.increaseHomeScore();
+      } else if (event.key == 'ArrowRight') {
+        this.increaseAwayScore();
+      } else if (event.key == 'ArrowUp') {
+        this.decreaseHomeScore();
+      } else if (event.key == 'ArrowDown') {
+        this.decreaseAwayScore();
+      }
+    }
+  }
+
+
   title = 'dc-scoreboard';
+  homescore = 0;
+  guestscore = 0;
+
+  formattedHomeScore = this.homescore.toLocaleString('en-US', {
+    minimumIntegerDigits: 2
+  });
+
+  formattedAwayScore = this.guestscore.toLocaleString('en-US', {
+    minimumIntegerDigits: 2
+  });
+
+  isSingleClick: Boolean = true;
+
+  increaseHomeScore = () => {
+    this.isSingleClick = true;
+    setTimeout(() => {
+      if (this.isSingleClick) {
+        this.homescore += 1;
+        this.formattedHomeScore = this.homescore.toLocaleString('en-US', {
+          minimumIntegerDigits: 2
+        });
+      }
+    }, 250)
+  };
+
+  decreaseHomeScore = () => {
+    this.isSingleClick = false;
+    this.homescore -= 1;
+    this.formattedHomeScore = this.homescore.toLocaleString('en-US', {
+      minimumIntegerDigits: 2
+    });
+  };
+
+  increaseAwayScore = () => {
+    this.isSingleClick = true;
+    setTimeout(() => {
+      if (this.isSingleClick) {
+        this.guestscore += 1;
+        this.formattedAwayScore = this.guestscore.toLocaleString('en-US', {
+          minimumIntegerDigits: 2
+        });
+      }
+    }, 250)
+  };
+
+  decreaseAwayScore = () => {
+    this.isSingleClick = false;
+    this.guestscore -= 1;
+    this.formattedAwayScore = this.guestscore.toLocaleString('en-US', {
+      minimumIntegerDigits: 2
+    });
+  };
+
+  openDialog(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      selectedTeam: this.selectedTeam,
+      teams: teams
+    };
+    const dialogRef = this.dialog.open(DialogAnimationsExampleDialog, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if(result.name != 'Neu'){
+        this.selectedTeam = result;
+      }else {
+        this.selectedTeam.name = result.newName;
+        this.selectedTeam.imageurl = result.imageurl;
+      }
+
+    });
+  }
+}
+
+@Component({
+  selector: 'changeGuestTeam',
+  templateUrl: 'app.changeGuestTeam.html',
+})
+export class DialogAnimationsExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private http: HttpClient
+  ) {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
-import { AppComponent } from './app.component';
+import {AppComponent, DialogAnimationsExampleDialog} from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {MatDialogModule} from "@angular/material/dialog";
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import { HttpClientModule } from '@angular/common/http';
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogAnimationsExampleDialog
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    MatSelectModule,
+    MatInputModule
+  ],
+  exports:[
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, DialogAnimationsExampleDialog]
 })
 export class AppModule { }
